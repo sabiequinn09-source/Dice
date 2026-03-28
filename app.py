c67e684d9d2f23e018db9b4b380eb7608d907b92
sabiequinn09-source <sabiequinn09@gmail.com>
2026-03-28 11:45:14 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def parse_and_roll(roll):
     roll=roll.replace(' ','')
     list1 = roll.split('d')
     list3 = []
+    list4= []
     total=0
     while True:
         list3=list1.copy()
@@ -23,32 +24,56 @@ def parse_and_roll(roll):
                 list1[i]=int(list1[i])
             except ValueError:
                 list2=list1[i].split('+')
-                list1[i]=list2[0]
-                for j in range(1,len(list2)):
-                    list1.append(list2[j])
-                for k in range(i+1,len(list1)-1):
-                    list1[k],list1[k+1]=list1[k+1],list1[k]
+                if len(list2)!=1:
+                    list1[i]=list2[0]
+                    for j in range(1,len(list2)):
+                        list1.append(list2[j])
+                    for k in range(i+1,len(list1)-1):
+                        list1[k],list1[k+1]=list1[k+1],list1[k]
+                else:
+                    list2=list1[i].split('-')
+                    for j in range(len(list2)):
+                        if j%2==0:
+                            list2[j]=int(list2[j])
+                        else:
+                            list2[j]=-int(list2[j])
+                    list1[i]=list2[0]
+                    for j in range(1,len(list2)):
+                        list1.append(list2[j])
+                    for k in range(i+1,len(list1)-1):
+                        list1[k],list1[k+1]=list1[k+1],list1[k]
         if list3==list1:
             break
     for i in range(0,len(list1)-1,2):
-        for k in range(list1[i]):
-            total+=r.randint(1,list1[i+1])
+        if list1[i] > 0:
+            for k in range(list1[i]):
+                x=r.randint(1,list1[i+1])
+                total+=x
+                list4.append(x)
+        else:
+            for k in range(0,list1[i],-1):
+                x=(-1) * r.randint(1,list1[i+1])
+                total+=x
+                list4.append(x)
     if len(list1)%2 != 0:
         total += list1[-1]
-    return total
+    return total, list4
 
 @app.route('/',methods=['GET', 'POST'])
 def index():
     result = None
+    rolls_list=[]
     roll_query = ''
     if request.method == 'POST':
         roll_query = request.form.get('roll')
         if roll_query:
             try:
-                result = parse_and_roll(roll_query)
+                result, rolls_list = parse_and_roll(roll_query)
             except:
                 result = 'Invalid Format! Try the format "xdy+zdw+...+a"'
-    return render_template('index.html', result=result, roll_query=roll_query)
+    return render_template('index.html', result=result, rolls_list=rolls_list, roll_query=roll_query)
+    
 
 if __name__ == '__main__':
+    #app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
     app.run(host='0.0.0.0', debug=True, port=5001, use_reloader=False)
