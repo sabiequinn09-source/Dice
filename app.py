825831b9f9afeba725c5263851d7159e0120b8f0
sabiequinn09-source <sabiequinn09@gmail.com>
2026-03-28 15:58:35 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -80,5 +80,5 @@ def index():
     
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
-    app.run(host='0.0.0.0', debug=True, port=5001, use_reloader=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
+    #app.run(host='0.0.0.0', debug=True, port=5001, use_reloader=False)
